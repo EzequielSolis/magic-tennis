33a4c4286f8ed7808af0e5e4319f1412f34901c2
Ezequiel <darkiel91@gmail.com>
2017-04-01 19:16:29 +0200

    campo nuevo

--- js/magicTennis.js
+++ js/magicTennis.js
@@ -12,6 +12,7 @@ estadoPrueba.preload = function(){
 	Kiwi.State.prototype.preload.call(this);
 	//añadir graficos
 	this.addImage("cesped", "graficos/cesped.png");
+	this.addImage("campo", "graficos/campo3.png");
 	this.addImage("bola", "graficos/bola.jpg");
 }
 
@@ -19,6 +20,7 @@ estadoPrueba.create = function() {
 	Kiwi.State.prototype.create.call(this);
 	//se colocan los graficos y los keybindings
 	this.fondo = new Kiwi.GameObjects.StaticImage (this, this.textures.cesped, 0, 0);
+	this.campo = new Kiwi.GameObjects.StaticImage (this, this.textures.campo, 150, -20);
 	this.bola = new Kiwi.GameObjects.StaticImage (this, this.textures.bola, 400, 140);
 
 	this.addChild(this.fondo);
