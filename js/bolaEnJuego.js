934d4d3df0c3904f46afa49f2a9746d7c8c3062d
Ezequiel <darkiel91@gmail.com>
2017-04-02 02:43:18 +0200

    campo nuevo y barras verticales
    las barras verticales estan mal hechas, habria que hacerlas horizontales
    y rotarlas

--- js/bolaEnJuego.js
+++ js/bolaEnJuego.js
@@ -2,7 +2,7 @@ bolaEnJuego.preload = function(){
 	//Kiwi.State.prototype.preload.call(this);
 	//añadir graficos
 	this.addImage("cesped", "graficos/cesped2.png");
-	this.addImage("campo", "graficos/campo5.png");
+	this.addImage("campo", "graficos/campo6.png");
 	this.addImage("bola", "graficos/bola.jpg");
 }
 
@@ -10,11 +10,11 @@ bolaEnJuego.create = function() {
 	//Kiwi.State.prototype.create.call(this);
 	//se colocan los graficos y los keybindings
 	this.fondo = new Kiwi.GameObjects.StaticImage (this, this.textures.cesped, 0, 0);
-	this.campo = new Kiwi.GameObjects.StaticImage (this, this.textures.campo, 0, 0);
-	this.bola = new Kiwi.GameObjects.StaticImage (this, this.textures.bola, 1000, 700);
+	this.campo = new Kiwi.GameObjects.StaticImage (this, this.textures.campo, 100, 0);
+	this.bola = new Kiwi.GameObjects.StaticImage (this, this.textures.bola, 500, 700);
 
-	this.campo.scaleX = 0.5;
-	this.campo.scaleY = 0.5; 
+	this.campo.scaleX = 1;
+	this.campo.scaleY = 1; 
 
 	//HUD
 	var puntuacion1 = "Jugador A: 15";
@@ -28,8 +28,8 @@ bolaEnJuego.create = function() {
 
 
     //barras  (game  current  max  x  y  [width=120]  [height=20]  [color='#000'] )
-	this.mana1 = new Kiwi.HUD.Widget.Bar(this.game, 100, 100, 20, 20, 300, 15, "blue" );
-	this.mana2 = new Kiwi.HUD.Widget.Bar(this.game, 100, 100, 20, 733, 300, 15, "blue" );
+	this.mana1 = new Kiwi.HUD.Widget.Bar(this.game, 100, 100, 20, 533, 15, 200, "blue" );
+	this.mana2 = new Kiwi.HUD.Widget.Bar(this.game, 100, 100, 984, 533, 15, 200, "blue" );
 
 
 	this.addChild(this.fondo);
@@ -42,8 +42,8 @@ bolaEnJuego.create = function() {
 	this.game.huds.defaultHUD.addWidget( this.mana2 );
 
 
-	this.game.cameras.defaultCamera.transform.x = -200;
-	this.game.cameras.defaultCamera.transform.y = -615;
+	this.game.cameras.defaultCamera.transform.x = 0;
+	this.game.cameras.defaultCamera.transform.y = -315;
 }
 
 bolaEnJuego.update = function() {
